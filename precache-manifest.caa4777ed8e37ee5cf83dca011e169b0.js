920c6196acef89973e1520e82f1dc997331809e3
wjmn <Jamin.Wu07@gmail.com>
2019-12-17 17:23:59 +0800

    Added build folder.

---
+++ precache-manifest.caa4777ed8e37ee5cf83dca011e169b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adef92240c2bc62748d9bbb294f5dd73",
+    "url": "/solve-24/index.html"
+  },
+  {
+    "revision": "38c1eb7b75cbef94ef81bcacb6838460",
+    "url": "/solve-24/manifest.json"
+  },
+  {
+    "revision": "b1902c67122f1dee9c15",
+    "url": "/solve-24/static/css/main.bf8335b9.chunk.css"
+  },
+  {
+    "revision": "b1902c67122f1dee9c15",
+    "url": "/solve-24/static/js/main.b1902c67.chunk.js"
+  },
+  {
+    "revision": "57c9853779df8d463d84",
+    "url": "/solve-24/static/js/runtime~main.57c98537.js"
+  },
+  {
+    "revision": "3ca81432027bbb7b376c",
+    "url": "/solve-24/static/js/vendors~main.3ca81432.chunk.js"
+  }
+]);
